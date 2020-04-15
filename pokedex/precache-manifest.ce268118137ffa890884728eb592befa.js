10b498ce59b1fe2d95cc8b1fa63d2500afe82587
Alexeev Konstantin <kalekseev@lab-code.com>
2020-04-15 13:31:55 +0500

    last release

---
+++ pokedex/precache-manifest.ce268118137ffa890884728eb592befa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c1e5257fe6d0761c405193302b24197",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46aa480b3ed3c6f8967e",
+    "url": "/static/css/main.62a82a34.chunk.css"
+  },
+  {
+    "revision": "bd0c22fe1ac4acd1a532",
+    "url": "/static/js/2.433ac47d.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.433ac47d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46aa480b3ed3c6f8967e",
+    "url": "/static/js/main.2786eac7.chunk.js"
+  },
+  {
+    "revision": "4d9af2a9620265095b18",
+    "url": "/static/js/runtime-main.e8bda96d.js"
+  }
+]);
